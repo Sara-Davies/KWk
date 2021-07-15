350dba7fe17c169160a44bad380bb4acee4007b5
Sara <saradavies41@gmail.com>
2021-07-15 00:29:20 -0400

    Complete Iteration 1

--- practice/appAboutMe/appAboutMe/ViewController.swift
+++ practice/appAboutMe/appAboutMe/ViewController.swift
@@ -9,6 +9,27 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    @IBAction func showMessage(_ sender: UIButton) {
+        let alertController = UIAlertController(title:"Fun Fact:", message:"I love macarons and sushi", preferredStyle: .alert)
+        alertController.addAction(UIAlertAction(title:"OK",style: UIAlertAction.Style.default, handler: nil ))
+        present(alertController, animated: true, completion: nil)
+    }
+    
+    
+    @IBAction func unicornMessage(_ sender: UIButton) {
+        let alertController = UIAlertController(title:"Fun Fact:", message:"My favorite animal is a leafy sea dragon", preferredStyle: .alert)
+        alertController .addAction(UIAlertAction(title:"OK",style:UIAlertAction.Style.default, handler: nil))
+        present(alertController, animated: true, completion:nil)
+    }
+    
+    
+    @IBAction func cancerMessage(_ sender: UIButton) {
+        let alertController = UIAlertController(title:"Fun Fact:", message:"My birthday is July 14th which is also Bastille Day", preferredStyle: .alert)
+        alertController .addAction(UIAlertAction(title:"OK",style:UIAlertAction.Style.default, handler: nil))
+        present(alertController, animated: true, completion:nil)
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
